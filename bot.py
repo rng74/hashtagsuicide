cfae44282b36d5ebc2e4c8e1e586bdda3c865bae
rng74 <ers98ers@gmail.com>
2020-05-06 16:28:22 +0600

    test commit

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ app = Client("694466049:AAFx8Ln9OmlfcX2dTjHuTnaznm2iAGqz9Lc")
 
 @app.on_message()
 def my_handler(client, message):
-	if ("#Важно" in message.text or "#важно" in message.text):
+	if ("#Важно" in message.text or "#важно" in message.text or "#test" in message.text):
 		app.send_message("hatersgonahate", message.text)
 
 app.run()
